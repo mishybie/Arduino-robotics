e37ac54278c1e78f4605a8fb1bc6d8cfa7a2282f
unknown <cwmitchelle@gmail.com>
2023-03-31 11:08:45 +0300

    Robotics2

---
+++ sketch_mar30a_copy_20230331105437/sketch_mar30a_copy_20230331105437.ino
@@ -0,0 +1,33 @@
+int num=40;
+
+
+
+void setup() {
+serial.begin(115200);
+
+  // put your setup code here, to run once:
+/*Blinh on LED 0n pin 9*/
+void setup(){
+  //setup pin modes(either output or input)
+  Pin mode(9,OUTPUT);
+  digitalwrite(9,HIGH);
+}
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Blink on LED 
+  void setup(0{
+    pinmode(10,OUTPUT);
+  }
+void loop(){
+  digitalwrite(10,HIGH);
+  delay(1000)
+  digitalwrite(10,LOW);
+  delay(1000);
+}
+
+  
+}
+
